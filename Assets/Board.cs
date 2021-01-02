153c5fec74f2ed60f978f2f8697bb1089de19f48
Cooper Collier <collier@berkeley.edu>
2021-01-02 03:39:09 -0800

    cleaned up

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -3,36 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using static System.Math;
-using System.Collections;
 
 public class Board : MonoBehaviour {
 
 	//--------------------------------------------------------------------------------
 
+    /* Instance of the main camera */
+    public Camera camera;
+
+    /* A hash table used to store all the dictionary words. */
+    Hashtable hashTable;
+
+    /* Two arrays describing the overall board dimensions, 8 wide and 24 tall. */
     static int totalRows = 24;
-    static int totalCols = 8;
+    static int totalCols = 8; 
 
-    /* Two arrays with the possible letters in the game and the score associated
-     * with that letter. */
-    char[] letters = new char[] {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-                                 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-                                 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    int[] scores = new int[] {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 
-                              5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 
-                              1, 4, 4, 8, 4, 10};
-    int[] amounts = new int[] {9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 
-                              1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 
-                              4, 2, 2, 1, 2, 1};               
+    /* All the possible letters in the game, according to the scrabble distributions. */
+    char[] letterDistribution = new char[] {
+     'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+     'B', 'B',
+     'C', 'C',
+     'D', 'D', 'D', 'D',
+     'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E',
+     'F', 'F',
+     'G', 'G', 'G',
+     'H', 'H',
+     'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I',
+     'J', 'J',
+     'K', 'K',
+     'L', 'L', 'L', 'L',
+     'M', 'M', 
+     'N', 'N', 'N', 'N', 'N', 'N',
+     'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+     'P', 'P',
+     'Q',
+     'R', 'R', 'R', 'R', 'R', 'R',
+     'S', 'S', 'S', 'S',
+     'T', 'T', 'T', 'T', 'T', 'T',
+     'U', 'U', 'U', 'U',
+     'V', 'V',
+     'W', 'W',
+     'X', 
+     'Y', 'Y',
+     'Z'
+    };
 
-	/* A 2D array holding the tile GameObjects for the board. The lower left corner
-	 * is (0, 0) and the upper right corner is (7, 7). */
+
+
+    /* The score associated with each letter in the game. */
+    int[] scores = new int[] {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
+                              1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+    //--------------------------------------------------------------------------------
+
+	/* A 2D array holding the tile GameObjects for the board. */
 	public Tile[,] tiles = new Tile[totalCols, totalRows];
 
 	/* Tile prefab to use for Instantiate() */
 	public Tile tile;
 
-    /* Instance of the main camera */
-    public Camera camera;
+    /* Two variables that need to be reported to the ButtonManager script. */
+    public static int score;
+    public static int movesRemaining;
+
+    /* Records the current state of the board at each turn. */
+    enum State { GetInput, TilesMoving, FindWords, ClearWords, DropTiles }
+    State currentState;
+
+    //--------------------------------------------------------------------------------
 
     /* Two tiles to swap when swapping letters */
     public Tile firstTile;
@@ -42,55 +80,50 @@ public class Board : MonoBehaviour {
     public int secondTileX;
     public int secondTileY;
 
-    /* Check wether the player has selected the first of 2 tiles to swap. */
+    /* Check wether the player has selected the first of 2 tiles to swap in GetInput(). */
     bool isTileSelected = false;
 
-    /* Check wether to delete a tile because it is forming a word. */
+    /* An array that holds a boolean to check if we want to delete a tile on
+     * this turn. There is one boolean for every tile on the board, but only
+     * the visible tiles are actually checked. */
     bool[,] toDelete = new bool[totalCols, totalCols];
 
-    /* Check is ClearWords() is currently running. */
-    bool coroutineStarted = false;
-
-    /* A hash table used to store all the dictionary words. */
-    Hashtable hashTable;
-
-    /* Records the current state of the board at each turn. */
-    enum State { GetInput, TilesMoving, FindWords, ClearWords, DropTiles }
-    State currentState;
-
-    public static int score;
-    public static int movesRemaining;
-    public static bool finished;
+    /* Check if ClearWords() is currently running, so it doesn't run twice. */
+    bool wordsAreBeingCleared = false;
 
     //--------------------------------------------------------------------------------
 
     void Start() {
+
+        /* Intialize the camera */
         camera = Camera.main;
+
+        /* Add all english words to the hashtable of words */
+        hashTable = new Hashtable();
+        string[] lines = System.IO.File.ReadAllLines("Assets/allWords.txt");
+        foreach (string line in lines) { hashTable.Add(line, true);}
+
+        /* Generate the board randomly, then remove any words that may have formed */
     	GenerateStartingBoard();
-    	hashTable = new Hashtable();
-    	string[] lines = System.IO.File.ReadAllLines("Assets/allWords.txt");
-    	foreach (string line in lines) {
-    		hashTable.Add(line, true);
-    	}
     	StartingLoop();
+
+        /* Set the current state */
         currentState = State.GetInput;
 
-        movesRemaining = 40;
-        finished = false;
+        /* Set the score and movesRemaining variables to their initial values. */
         score = 0;
+        movesRemaining = 40;
+
     }
 
     //--------------------------------------------------------------------------------
 
     void Update() {
 
-        if (movesRemaining <= 0 && currentState == State.GetInput) {
-            finished = true;
-            return;
-        }
-
-    	if (Input.GetMouseButtonDown(0) && (currentState == State.GetInput)) {
-    		GetInput();  
+    	if (currentState == State.GetInput) {
+            if (Input.GetMouseButtonDown(0)) {
+    		    GetInput();  
+            }
         }
 
         if (currentState == State.TilesMoving) {
@@ -105,13 +138,12 @@ public class Board : MonoBehaviour {
         	}
         }
 
-        if (currentState == State.ClearWords && !coroutineStarted) {
+        if (currentState == State.ClearWords && !wordsAreBeingCleared) {
         	StartCoroutine(ClearWords());
         }
 
         if (currentState == State.DropTiles) {
         	DropTiles();
-        	currentState = State.TilesMoving;
         }
 
     }
@@ -120,37 +152,52 @@ public class Board : MonoBehaviour {
 
     void GetInput() {
 
+        /* Determine where the mouse click happened */
     	Vector3 position = camera.ScreenToWorldPoint(Input.mousePosition);
         Vector2 shiftedPosition = new Vector2(position.x + 32, position.y + 32);
         int locationX = (int) (shiftedPosition.x / 32);
         int locationY = (int) (shiftedPosition.y / 32);
 
+        /* Check if the mouse click was within the bounds of the board. */
         if ((locationX >= 0 && locationX < totalCols) && (locationY >= 0 && locationY < totalCols)) {
 
+            /* Tell the script which tile the player clicked on */
             Tile selectedTile = tiles[locationX, locationY];
             if (selectedTile == null) { return; }
 
+            /* This section runs of this it the second of two clicks. */
             if (isTileSelected) {
 
+                /* Prepare to swap the tiles. */
             	firstTile.GetComponent<SpriteRenderer>().color = Color.white;
                 isTileSelected = false;
                 secondTile = selectedTile;
                 secondTileX = selectedTile.locationX;
                 secondTileY = selectedTile.locationY;
 
+                /* Ensure the two tiles can be swapped, then swap them. */
                 if ((System.Math.Abs(secondTileX - firstTileX) 
                     + System.Math.Abs(secondTileY - firstTileY)) == 1) {
+
+                    /* Tell the two tiles to update their own positions. */
                     firstTile.SendMessage("Move", new int[]{secondTileX, secondTileY});
                     secondTile.SendMessage("Move", new int[]{firstTileX, firstTileY});
+
+                    /* Update the positions on the tiles array. */
                     tiles[firstTileX, firstTileY] = secondTile;
                     tiles[secondTileX, secondTileY] = firstTile;
-                    currentState = State.TilesMoving;
+
+                    /* Decrement the moves remaining, and change the state to TilesMoving. */
                     movesRemaining -= 1;
+                    currentState = State.TilesMoving;
+
                 }
 
+                /* Set the two tiles to null after the swap has completed. */
                 firstTile = null;
                 secondTile = null;
 
+            /* This section runs if it is the first of two clicks. */
             } else {
                 isTileSelected = true;
                 firstTile = selectedTile;
@@ -160,10 +207,13 @@ public class Board : MonoBehaviour {
             }
 
         }
+
     }
 
     //--------------------------------------------------------------------------------
 
+    /* Continually repeats this functions and waits until every tile reports that
+     * it is finished moving. */
     void TilesMoving() {
     	for (int row = 0; row < totalRows; row += 1) {
     		for (int col = 0; col < totalCols; col += 1) {
@@ -180,18 +230,18 @@ public class Board : MonoBehaviour {
 
     //--------------------------------------------------------------------------------
 
+    /* Check if there are any words formed on the board. If there are, record them in 
+     * the ToDelete array and return "true". */
     bool FindWords() {
 
+        /* set the toDelete array to be all blank, and initialize the return value. */
     	toDelete = new bool[totalCols, totalCols];
-
     	bool returnValue = false;
 
     	string bestString = "";
         int bestStringLength = 0;
         string testString = "";
         int testStringLength = 0;
-
-        //TODO: Fix 'TWOW' detection?
 
         for (int row = 0; row < totalCols; row += 1) {
 
@@ -241,67 +291,100 @@ public class Board : MonoBehaviour {
     	return returnValue;
 
         // TODO: Add bonus multipliers for long words, or for eight-letter words.
+        // TODO: Fix 'TWOW' detection?
 
     }
 
     //--------------------------------------------------------------------------------
 
+    /* Delete tiles from the board that are forming words. This is a 
+     * Coroutine because the board needs to wait for a second to show which
+     * words are being cleared. */
     IEnumerator ClearWords() {
-    	coroutineStarted = true;
+
+    	wordsAreBeingCleared = true;
+
     	yield return new WaitForSeconds(1);
+
     	for (int row = 0; row < totalCols; row += 1) {
     		for (int col = 0; col < totalCols; col += 1) {
+
     			if (toDelete[col, row]) {
     				Tile tileToDelete = tiles[col, row];
                     score += tileToDelete.points;
     				Destroy(tileToDelete.gameObject);
     				tiles[col, row] = null;
     			}
+
     		}
     	}
+
     	toDelete = new bool[totalCols, totalCols];
     	currentState = State.DropTiles;
-    	coroutineStarted = false;
+
+    	wordsAreBeingCleared = false;
+
     }
 
     //--------------------------------------------------------------------------------
 
+    /* Now that some tiles have been cleared from the board, there will be
+     * a few tiles left 'hanging' in the air. 
+     * This functions tells those hanging tiles to fall into the highest
+     * tile below them. */
     void DropTiles() {
+
     	for (int row = 1; row < totalRows; row += 1) {
     		for (int col = 0; col < totalCols; col += 1) {
 
     			if (tiles[col, row] != null && tiles[col, row-1] == null) {
+
     				Tile tileToMove = tiles[col, row];
     				int groundRow;
+
     				for (groundRow = row; groundRow >= 0; groundRow -= 1) {
     					if (groundRow == 0) { break; }
     					if (tiles[col, groundRow-1] != null) { break; }
     				}
+
     				tileToMove.SendMessage("Move", new int[]{col, groundRow});
     				tiles[col, row] = null;
     				tiles[col, groundRow] = tileToMove;
+
     			}
 
     		}
     	}
+
+        currentState = State.TilesMoving;
+
     }
 
     //--------------------------------------------------------------------------------
 
     /* Make sure there are no words at the start of the game. */
     void StartingLoop() {
-    	for (int i = 0; i < (totalCols-3); i += 1) {
+
+        int repetitions = 10;
+
+    	for (int i = 0; i < repetitions; i += 1) {
+
     		FindWords();
+
     		for (int row = 0; row < totalCols; row += 1) {
     			for (int col = 0; col < totalCols; col += 1) {
+
     				if (toDelete[col, row]) {
     					Tile tileToDelete = tiles[col, row];
     					Destroy(tileToDelete.gameObject);
     					tiles[col, row] = GenerateRandomTile(col, row);
     				}
+
     			}
     		}
+
     	}
+
     }
 
     /* Randomly generate the board's starting state. */
@@ -316,16 +399,10 @@ public class Board : MonoBehaviour {
     /* Pick a random letter tile. */
     Tile GenerateRandomTile(int locationX, int locationY) {
     	Tile newTile = (Tile) Instantiate(tile);
-        int randomNumber1 = Random.Range(0, 26);
-        int randomNumber2 = Random.Range(0, 26);
-        int randomNumber;
-        if (amounts[randomNumber2] > amounts[randomNumber1]) {
-        	randomNumber = randomNumber2;
-        } else {
-        	randomNumber = randomNumber1;
-        }
-    	newTile.letter = letters[randomNumber];
-    	newTile.points = scores[randomNumber];
+        int randomNumber = Random.Range(0, 100);
+    	newTile.letter = letterDistribution[randomNumber];
+        int positionInAplhabet = (int) newTile.letter - 64;
+    	newTile.points = scores[positionInAplhabet - 1];
     	newTile.locationX = locationX;
     	newTile.locationY = locationY;
     	return newTile;
@@ -340,16 +417,22 @@ public class Board : MonoBehaviour {
     	}
     }
 
-    public int reportScore() {
+    //--------------------------------------------------------------------------------
+
+    public int ReportScore() {
         return score;
     }
 
-    public int reportMoves() {
+    public int ReportMoves() {
         return movesRemaining;
     }
 
-    public bool reportFinished() {
-        return finished;
+    public bool CheckIfGameFinished() {
+        if (movesRemaining <= 0 && currentState == State.GetInput) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //--------------------------------------------------------------------------------
--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -12,49 +12,59 @@ public class Tile : MonoBehaviour {
 	/* The number of points gained by using this letter. */
 	public int points;
 	/* The (x, y) coordinate of this tile on the board. */
-	public int locationX;
-	public int locationY;
+	public int locationX, locationY;
 
 	/* GameObject holding this tile's text, and the corresponding text component. */
 	public GameObject textObject;
 	public Text text;
 
-    /* True if the tile is currently moving. */
-    public bool moving = false;
-
     /* Check wether the game has started. The tile sets its letter and position beforehand. */
     bool gameStarted = false;
 
     /* Used if moving = true, to get the tile to its destination. */
-    float destinationX;
-    float destinationY;
+    float destinationX, destinationY;
+
+    /* True if the tile is currently moving. */
+    public bool moving = false;
+
     /* Speed multiplier to use while moving. */
-    float speed = 3f;
+    static float speed = 3f;
+    /* Distance to the destination where it's small enough to just finish moving. */
+    static float minimumDistance = 2f;
 
     //--------------------------------------------------------------------------------
 
     void Update() {
 
         if (!gameStarted) {
+
             textObject = transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
             text = textObject.GetComponent<Text>();
             text.text = letter.ToString();
+
             transform.position = new Vector3(TileToWorldSpace(locationX), TileToWorldSpace(locationY), 0);
             gameStarted = true;
+
         }
 
         if (moving) {
-            Vector2 direction = new Vector2(destinationX - transform.position.x,
-                                            destinationY - transform.position.y);
-            if (direction.magnitude < 2f) {
+
+            float xDirection = destinationX - transform.position.x;
+            float yDirection = destinationY - transform.position.y;
+            Vector2 direction = new Vector2(xDirection, yDirection);
+
+            if (direction.magnitude < minimumDistance) {
                 moving = false;
                 transform.position = new Vector3(destinationX, destinationY, 0);
             } else {
                 transform.Translate(direction * speed * Time.deltaTime);
             }
+
         }
 
     }
+
+    //--------------------------------------------------------------------------------
 
     /* Turns a coordinate like [3, 4] into a coordinate like [127.3, 224.8] */
     float TileToWorldSpace(int location) {
@@ -70,8 +80,6 @@ public class Tile : MonoBehaviour {
         destinationX = TileToWorldSpace(destination[0]);
         destinationY = TileToWorldSpace(destination[1]);
         moving = true;
-
-        //Debug.Log(letter.ToString() + " " + destination[0].ToString() + destination[1].ToString());
     }
 
     //--------------------------------------------------------------------------------
